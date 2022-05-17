04e0d6fba496125330e1f4f6b3f5d2d33d57f6c7
Chi-Hung Chang <jack7989@gmail.com>
2022-05-16 23:22:24 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -16,7 +16,7 @@ import branca.colormap as cm
 today_date = datetime.date.today()
 today_datestr = today_date.strftime('%Y-%m-%d')
 exp_mid_fct = MediumRange(station_id=7469342)
-exp_mid_fct_data = pd.DataFrame(exp_mid_fct.data['mean'][0]['data'])['forecast-time']
+exp_mid_fct_data = pd.DataFrame(exp_mid_fct.data["mean"][0]["data"])["forecast-time"]
 exp_mid_fct_time = pd.to_datetime(exp_mid_fct_data)
 
 last_date = exp_mid_fct_time[len(exp_mid_fct_time)-1]
