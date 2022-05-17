12903bd6bd3a7dd0debe86e645516cb7d8fb1d68
Chi-Hung Chang <jack7989@gmail.com>
2022-05-16 20:41:31 -0500

    Update syn_noaa.py

--- syn_noaa.py
+++ syn_noaa.py
@@ -7,7 +7,7 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.colors
-import pynwm
+from pynwm_upd import *
 import datetime as dt
 import pandas as pd
 
