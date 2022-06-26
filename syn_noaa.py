417ab9573ae9252c1eafd7de14d83ae59dc95e1d
Chi-Hung Chang <jack7989@gmail.com>
2022-06-25 21:42:06 -0500

    Update syn_noaa.py

--- syn_noaa.py
+++ syn_noaa.py
@@ -10,7 +10,7 @@ import matplotlib.colors
 import requests
 import datetime as dt
 import pandas as pd
-from scipy import interpolate
+#from scipy import interpolate
 
 
 def perf_qm(org_stack, syn_stack, qm_stack, qm_type=0, nbins=100):
@@ -91,13 +91,13 @@ def perf_qm(org_stack, syn_stack, qm_stack, qm_type=0, nbins=100):
                 crt = np.interp(bin_unc_mdl, binmid, bias[:, ct_r, ct_c])
 
                 temp = qm_syn + crt
-                #temp[temp>100] = 100
-                #temp[temp<0] = 0
+                temp[temp>100] = 100
+                temp[temp<0] = 0
 
-                #map_syn[:,ct_r,ct_c] = temp
+                map_syn[:,ct_r,ct_c] = temp
                 
-    map_syn = np.where(map_syn>100,100,map_syn)
-    map_syn = np.where(map_syn<0,0,map_syn)        
+    #map_syn = np.where(map_syn>100,100,map_syn)
+    #map_syn = np.where(map_syn<0,0,map_syn)        
 
     return map_syn
 
